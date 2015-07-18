e998ad9320e0fed584c1d9028190564181b67ef0
Wayne Crasta <wayne.crasta@gmail.com>
2015-07-18 15:41:38 -0700

    Yelp API

--- search_by_radius.py
+++ search_by_radius.py
@@ -1,8 +1,14 @@
 import cherrypy
 import requests
 import json
+import oauth2
 
-API_KEY = 'AIzaSyCY0yYTShIG54l8rUPNUOsl3Jm7NdWtXBQ'
+# API_KEY = 'AIzaSyCY0yYTShIG54l8rUPNUOsl3Jm7NdWtXBQ'
+API_HOST = 'http://api.yelp.com/v2/search/?'
+CONSUMER_KEY = 'bH-ntL7Nv-iUC7mndRvujw'
+CONSUMER_SECRET = 'B-Br6RdJJpyMZAwYAmoKPisv-Cw'
+TOKEN = 'ugQuBYYmoJBNcnBDpVrVO2HiPpMyahmZ'
+TOKEN_SECRET = 'ljnrQcblPl7yHbuQebRoqhvlRoI'
 
 
 class HelloWorld(object):
@@ -13,22 +19,38 @@ class HelloWorld(object):
                 <fieldset>
                 <legend>Polling:</legend>
                 Radius in Miles:<br>
-                <input type="text" name="miles" value="">
+                <input type="text" name="miles" value="5"> <br>
+                Category:<br>
+                <input type="radio" name="category" value="bbq">Barbeque<br>
+                <input type="radio" name="category" value="pizza">Pizza<br>
+                <input type="radio" name="category" value="burgers">Burgers<br>
                 <br><br>
                 <input type="submit" value="Submit"></fieldset>
                 </form>'''
 
     @cherrypy.expose
-    def search(self):
-        # meters = float(miles) * 1609.34
-        # url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.638088,-96.370452&radius={}&types=restaurant&key={}'.format(meters, API_KEY)
-        url = 'http://localhost:5588/results'
-        response = requests.get(url)
-        results = json.loads(response.text)['results']
-        for result in results:
-            name = result['name']
-            address = result['vicinity']
-            yield '{}: {} </br>'.format(name, address)
+    def search(self, miles, category):
+        location = 'Bryan, TX'
+        meters = float(miles) * 1609.34
+        consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+        oauth_request = oauth2.Request(method="GET", url=API_HOST, parameters=None)
+
+        oauth_request.update(
+            {
+                'oauth_nonce': oauth2.generate_nonce(),
+                'oauth_timestamp': oauth2.generate_timestamp(),
+                'oauth_token': TOKEN,
+                'oauth_consumer_key': CONSUMER_KEY,
+                'location': location,
+                'category_filter': category,
+                'radius_filter': meters
+            }
+        )
+        token = oauth2.Token(TOKEN, TOKEN_SECRET)
+        oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+        signed_url = oauth_request.to_url()
+        response = requests.get(signed_url)
+        yield response.text
 
     @cherrypy.expose
     def results(self):
