4023a342f44cd1286c32e0a050f314cf4340d325
Samantha Cameron <samanthacameron@go.rmc.edu>
2015-07-18 18:29:33 -0700

    changes

--- poll.py
+++ poll.py
@@ -1,8 +1,23 @@
 import cherrypy
-from database import session
 import os, os.path
 import random
 import string
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_declarative import Restaurant, User
+
+Base = declarative_base()
+
+user = 'texas'
+password = 'texas'
+database_host = 'hacksql.viasat.io'
+engine = create_engine(
+    'mysql+mysqlconnector://{}:{}@{}/texas'.format(user, password, database_host))
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+metadata = MetaData()
 
 class Poll(object):
     def __init__(self):
