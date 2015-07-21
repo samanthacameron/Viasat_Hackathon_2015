f046db7a347ad41b9781e2932d578c09bf704f2a
wcrasta <Wayne.Crasta@viasat.com>
2015-07-21 09:24:17 -0500

    Updated sql file

--- database.py
+++ database.py
@@ -24,9 +24,11 @@ class User(Base):
     rest_id = Column(Integer, ForeignKey('restaurants.id'), nullable=True)
     voted = Column(Boolean, nullable=False)
 
+
 class UserList(Base):
     __tablename__ = 'userlist'
     username = Column(String(50), primary_key=True)
+
 
 class Blacklist(Base):
     __tablename__ = 'blacklist'
@@ -35,11 +37,11 @@ class Blacklist(Base):
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
 
-user = 'texas'
-password = 'texas'
-database_host = 'hacksql.viasat.io'
+user = 'wayne'
+password = 'password'
+database_host = '10.0.1.209'
 engine = create_engine(
-    'mysql+mysqlconnector://{}:{}@{}/texas'.format(user, password, database_host))
+    'mysql+mysqlconnector://{}:{}@{}/hackathon'.format(user, password, database_host))
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
--- restaurants.sql
+++ restaurants.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.6.25-log - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             9.2.0.4947
+-- Host:                         hacksql.viasat.io
+-- Server version:               5.5.43-0ubuntu0.14.04.1 - (Ubuntu)
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             9.1.0.4867
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,23 +10,101 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping database structure for hackathon
-DROP DATABASE IF EXISTS `hackathon`;
-CREATE DATABASE IF NOT EXISTS `hackathon` /*!40100 DEFAULT CHARACTER SET utf8 */;
+-- Dumping database structure for texas
+CREATE DATABASE IF NOT EXISTS `hackathon` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `hackathon`;
 
 
--- Dumping structure for table hackathon.restaurants
-DROP TABLE IF EXISTS `restaurants`;
-CREATE TABLE IF NOT EXISTS `restaurants` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `category` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Dumping structure for table texas.blacklist
+CREATE TABLE IF NOT EXISTS `blacklist` (
+  `name` varchar(100) NOT NULL DEFAULT 'McDonalds',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table texas.blacklist: ~1 rows (approximately)
+/*!40000 ALTER TABLE `blacklist` DISABLE KEYS */;
+INSERT INTO `blacklist` (`name`) VALUES
+	('McDonalds');
+/*!40000 ALTER TABLE `blacklist` ENABLE KEYS */;
+
+
+-- Dumping structure for table texas.restaurants
+CREATE TABLE IF NOT EXISTS `restaurants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(350) NOT NULL,
+  `category` varchar(50) NOT NULL,
+  `votes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table texas.restaurants: ~8 rows (approximately)
+/*!40000 ALTER TABLE `restaurants` DISABLE KEYS */;
+INSERT INTO `restaurants` (`id`, `name`, `address`, `category`, `votes`) VALUES
+	(58, 'Chick-fil-A', '2210 Briarcrest Dr Bryan, TX 77802 ', 'None', 1),
+	(63, 'Fazolis', '400 Harvey Rd College Station, TX 77840 ', 'italian', 1),
+	(66, 'Grub Burger Bar', '980 University Dr E Ste 400 College Station, TX 77840 ', 'burgers', 3),
+	(72, 'Harvey Washbangers', '1802 Texas Ave S College Station, TX 77840 ', 'burgers', 3),
+	(86, 'Cicis Pizza', '1921 S Texas Ave Bryan, TX 77802 ', 'pizza', 3),
+	(89, 'Carters Burger', '3105 Texas Ave Bryan, TX 77802 ', 'burgers', 1),
+	(90, 'Rudys Country Store And Bar-B-Q', '504 Harvey Rd College Station, TX 77840 ', 'bbq', 2),
+	(93, 'La Familia Taqueria', '300 N Texas Ave Bryan, TX 77803 ', 'mexican', 0);
+/*!40000 ALTER TABLE `restaurants` ENABLE KEYS */;
+
+
+-- Dumping structure for table texas.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(50) NOT NULL,
+  `rest_id` int(11) DEFAULT NULL,
+  `voted` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`username`),
+  KEY `FK_user_restaurants` (`rest_id`),
+  CONSTRAINT `FK_user_restaurants` FOREIGN KEY (`rest_id`) REFERENCES `restaurants` (`id`),
+  CONSTRAINT `FK_user_userlist` FOREIGN KEY (`username`) REFERENCES `userlist` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table texas.user: ~13 rows (approximately)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`username`, `rest_id`, `voted`) VALUES
+	('bojeda', 90, 1),
+	('cabney', 66, 1),
+	('ccatalena', NULL, 0),
+	('clenzen', 66, 1),
+	('dayers', 90, 1),
+	('dgilman', 72, 1),
+	('jross', 86, 1),
+	('lrutledge', 72, 1),
+	('newperson', NULL, 0),
+	('rmilbourne', NULL, 0),
+	('scameron', 72, 1),
+	('soneal', 89, 1),
+	('wcrasta', 86, 1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+
+-- Dumping structure for table texas.userlist
+CREATE TABLE IF NOT EXISTS `userlist` (
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table texas.userlist: ~13 rows (approximately)
+/*!40000 ALTER TABLE `userlist` DISABLE KEYS */;
+INSERT INTO `userlist` (`username`) VALUES
+	('bojeda'),
+	('cabney'),
+	('ccatalena'),
+	('clenzen'),
+	('dayers'),
+	('dgilman'),
+	('jross'),
+	('lrutledge'),
+	('newperson'),
+	('rmilbourne'),
+	('scameron'),
+	('soneal'),
+	('wcrasta');
+/*!40000 ALTER TABLE `userlist` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
